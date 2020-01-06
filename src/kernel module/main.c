ac8595d24a293efd354cc8965b1c664dbe41479b
Fabian Ulbricht <40840365+FennekTheFox@users.noreply.github.com>
2020-01-06 10:38:16 +0100

    documentation

--- src/kernel module/main.c
+++ src/kernel module/main.c
@@ -27,62 +27,96 @@ volatile int sem = 0;
 static struct task_struct *worker_task;
 static int get_current_cpu;
  
+
+// Variables that can be changed from userspace
+// by writing in the corresponding files in
+// sys/module/<modname>/parameters/<parameter name>
 module_param(duty, int, 0644);
 module_param(frequency, int, 0644);
 module_param(enable, int, 0644);
  
 /*--------------------------------------------------*/
 /*          Configure the GPIO port(s)              */
- 
+/*
+*@brief: Function that initialises a GPIO pin for use as PWM
+*/
 int __init pwm_gpio_init(void)
 {
+	//initialises defined gpio pin as output for use as soft-pwm
 	printk(KERN_INFO "PWM: starting %s. \n", __FUNCTION__);
 	gpio_request(pwm1, "pwm1");
 	gpio_direction_output(pwm1, 0);
 	
 	return 0;
 }
- 
+
+/*
+*@brief: Function that frees the pin used for the pwm
+*/
 void __exit pwm_gpio_exit(void)
 {
+	// frees the gpio pin again
 	printk(KERN_INFO "PWM: stopping %s.\n", __FUNCTION__);
 	gpio_free(pwm1);
 }
 /*--------------------------------------------------*/
 /*           Run PWM on the GPIO port               */
  
+/*
+*@brief: Function for the worker thread to implement the Soft-PWM
+*/
  static int worker_task_handler_fn(void *arguments)
 {
+	 // This is the function being threaded by the module
+	 // It handles the Soft-PWM by setting the GPIO pin
+	 // to HIGH, sleeping for the (1/frequency/100)*duty
+	 // then setting it to LOW and sleeping for
+	 // (1/frequency/100)*(100-duty)
 	int tusec_On;
 	int tusec_Off;
 	
 	//sem=1;
 	
+	 //Allows this thread to be killed by the parent thread
 	allow_signal(SIGKILL);
 	
+	//As long as the thread isnt killed from the outside do...
 	while(!kthread_should_stop()){
 		/* Calculate from frequency and dutycycle the delay-times */
 		
 		tusec_On  = (1000000*duty)/(frequency*100);			// Duration of on-cycle
 		tusec_Off = (1000000*(100-duty))/(frequency*100);	// Duration of off-cycle
 		printk("duty:%d, tusec_on:%d, tusec_off:%d, freq:%d", duty, tusec_On, tusec_Off, frequency);
+		//se thigh and sleep
 		gpio_set_value(pwm1, 1);
 		usleep_range(tusec_On, tusec_On);
+		//set low and sleep
 		gpio_set_value(pwm1, 0);
 		usleep_range(tusec_Off, tusec_Off);
-
+		
+		//End thread if theres an unhandled signal pending
 		if (signal_pending(worker_task))
 			            break;
 	}
-	//sem=0;
+	
+	// Function provided in linux/kernel to properly end a process in kernel mode
 	do_exit(0);
 
 	pr_alert("Worker task exiting\n");
 	return 0;
 }
 
+/*
+*brief: Function to start the Soft-PWM via a separate thread on the specified pin
+*@return: 0, additional information in dmesg
+*/
 int pwn_run_init(void)
 {
+	//For proper results the Soft-PWM must be prioritize
+	// otherwise in a high-workload-situation the htread
+	// for the soft-PWM might be ignored and the fan stops
+	// causing temperatures to rise and thermal throtteling
+	// which in turn reducec performance, making everything worse
 	struct sched_param task_sched_params =
 	{
 			.sched_priority = MAX_RT_PRIO
@@ -90,7 +124,7 @@ int pwn_run_init(void)
 	
 	printk(KERN_ALERT "STARTING PWM: Frequency is %dMHz, and dutycycle is %d percent.\n", frequency, duty);
 	
-	//Start thread
+	//Start thread with some debug information about which CPU core handles the workload
 
 	task_sched_params.sched_priority = 90;
 	printk("Creating PWM thread\n");
@@ -109,6 +143,9 @@ int pwn_run_init(void)
 	return 0;
 }
  
+/*
+*brief: Function that terminates the PWM thread
+*/
 void pwm_run_exit(void)
 {
 	printk(KERN_ALERT "STOPPING PWM in %s.\n", __FUNCTION__);
@@ -126,7 +163,7 @@ void pwm_run_exit(void)
 int __init pwm_init(void)
 {
 	printk(KERN_INFO " Starte the %s function.\n", __FUNCTION__);
-	/* Configure initial state of the PWM pins */
+	/* Configure initial state of the PWM pins and start it*/
 	pwm_gpio_init();
 	pwn_run_init();
  
